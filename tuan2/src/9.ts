6c0528b43a7781d099ca95bdc942eedd1e2d4ec7
imthaivu <imquocthai@gmail.com>
2025-08-30 14:43:15 +0700

    tuan2 9

---
+++ tuan2/src/9.ts
@@ -0,0 +1,10 @@
+function filterEvens(arr: number[]): Promise<number[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(arr.filter((n) => n % 2 === 0));
+    }, 1000);
+  });
+}
+
+filterEvens([1, 2, 3, 4, 5, 6])
+  .then((evens) => console.log("Even nums:", evens));
